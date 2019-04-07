308a71306d6e7318bc692e4a6bdd94bb9cbf83c7
Loup Vaillant <loup@loup-vaillant.fr>
2019-04-07 15:16:55 +0200

    Annotated the tests

--- src/test.ml
+++ src/test.ml
@@ -1,14 +1,14 @@
-open Proto
-
-let validate str =
+let protocol_of_string str =
   str
   |> Lexing.from_string
   |> Scan.tokens
   |> Parsec.protocol
-  |> Validate.v
+
+let validate str =
+  Validate.v (protocol_of_string str)
 
 let _ = List.map validate
-          [ "<- s ... -> e    <- e, ee, es  -> s, se"
+          [ "<- s ... -> e    <- e, ee, es  -> s, se"      (* XK1 *)
           ; "         -> e    <- e, ee, s   -> s, es, se"
           ; "         -> e, e"
           ; "         -> s, s"
@@ -16,10 +16,11 @@ let _ = List.map validate
           ; "         -> e    <- s, s"
           ; "         -> e    <- e, ee, es  -> s, se"
           ; "         -> e    <- e, ee, ee  -> s, se"
-          ; "         -> e    <- e          -> ee"
+          ; "         -> e    <- e          -> ee"         (* valid!! *)
           ; "         -> e    -> e, ee"
           ; "<- e ... -> e, ee"
           ; "<- s -> s, ss ... -> e, es"
           ; ""
           ; "<- s ... -> e <- e, ee, es -> s"
           ]
+
